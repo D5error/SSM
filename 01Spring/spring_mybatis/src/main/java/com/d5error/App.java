b751a724b391fb9c3778909b99fd6d73d449731b
D5error <D5error@163.com>
2024-08-14 10:27:09 +0800

    2024/08/13

---
+++ 01Spring/spring_mybatis/src/main/java/com/d5error/App.java
@@ -0,0 +1,37 @@
+package com.d5error;
+
+
+import com.d5error.dao.AccountDao;
+import com.d5error.domain.Account;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        AccountDao accountDao = sqlSession.getMapper(AccountDao.class);
+
+        Account d5 = new Account();
+        d5.setName("d5error");
+        d5.setMoney(114514);
+
+//        Account account = accountDao.findById(1);
+        accountDao.save(d5);
+        List<Account> account = accountDao.findAll();
+
+        System.out.println(account);
+
+        sqlSession.close();
+
+    }
+}
