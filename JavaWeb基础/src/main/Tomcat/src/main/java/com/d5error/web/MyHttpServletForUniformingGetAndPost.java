43ab4e8ffb9c1d98029043499483a6fcba356748
D5error <D5error@163.com>
2024-10-05 22:43:12 +0800

    servlet解决get和post的中文乱码问题

--- JavaWeb基础/src/main/Tomcat/src/main/java/com/d5error/web/MyHttpServletForUniformingGetAndPost.java
+++ JavaWeb基础/src/main/Tomcat/src/main/java/com/d5error/web/MyHttpServletForUniformingGetAndPost.java
@@ -6,19 +6,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @WebServlet("/GetAndPost")
 public class MyHttpServletForUniformingGetAndPost extends HttpServlet {
+    boolean isPost = false;
+
     @ Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("收到一条get请求");
+
+        req.setCharacterEncoding("UTF-8");
 
         Map<String, String[]> parameterMap = req.getParameterMap();
         for (String key : parameterMap.keySet()) {
             System.out.print(key + ":" );
             String[] values = parameterMap.get(key);
             for (String value : values) {
+                if (!isPost) {
+                    byte[] bytes = value.getBytes(StandardCharsets.ISO_8859_1);
+                    value = new String(bytes, StandardCharsets.UTF_8);
+                }
                 System.out.print(value + " " );
             }
             System.out.println();
@@ -27,6 +36,7 @@ public class MyHttpServletForUniformingGetAndPost extends HttpServlet {
 
     @ Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        isPost = true;
         System.out.println("收到了一条post请求");
         System.out.println("转发至doGet方法");
         this.doGet(req, resp);
